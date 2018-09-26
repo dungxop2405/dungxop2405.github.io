44385ac9ee25cd775671755688188dd3204f6013
dungxop2405 <34085451+dungxop2405@users.noreply.github.com>
2018-09-26 20:22:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,68 @@
-const socket = io('http://localhost:3000');
-function openStream(){
-	const config = { audio: false, video: true };
-	return navigator.mediaDevices.getUserMedia(config);
-}
+client = new Paho.MQTT.Client("m21.cloudmqtt.com", 31277, "web_" + parseInt(Math.random() * 100, 10));
 
-function playStream(idVideoTag, stream){
-	const video = document.getElementById(idVideoTag);
-	video.srcObject = stream;
-	video.play();
-}
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  var options = {
+    useSSL: true,
+    userName: "ygfhpcim",
+    password: "YNwI3tVF4imo",
+    onSuccess:onConnect,
+    onFailure:doFail
+  }
 
-openStream()
-.then(stream => playStream('localStream', stream));
+  // connect the client
+  client.connect(options);
+
+  // called when the client connects
+  function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+    client.subscribe("demo");
+    message = new Paho.MQTT.Message("Hello CloudMQTT");
+    message.destinationName = "demo";
+    client.send(message);
+  }
+
+  function doFail(e){
+    console.log(e);
+  }
+
+  // called when the client loses its connection
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+  }
+
+  // called when a message arrives
+  function onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+  }
+
+function toggleSonon() {
+			message = new Paho.MQTT.Message("toggleSonon");
+		    message.destinationName = "demo";
+		    client.send(message);
+            console.log("toggleSonon");        
+        };
+
+function toggleSonoff() {
+			message = new Paho.MQTT.Message("toggleSonoff");
+		    message.destinationName = "demo";
+		    client.send(message);
+            console.log("toggleSonoff");        
+        };
+
+$(document).ready(function(){
+	$('#on').on('change',function(){
+    	if(this.checked){
+    		$("#info").text("U checked me, place some code here");
+    		toggleSonon();
+    	}
+        else{
+        	$("#info").text("U unchecked me, another piece of code here");
+        	toggleSonoff();
+        }
+    });
+});
